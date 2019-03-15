8f3a820e1a0997c9cb9b5568634f03db55666e14
Bahirau Ruslan <ruslan.bahirau.1999@gmail.com>
2019-03-15 10:42:38 +0300

    Added authentication util

--- src/main/java/by/bntu/fitr/povt/bahirauruslan/facultative/models/services/guest/AccountService.java
+++ src/main/java/by/bntu/fitr/povt/bahirauruslan/facultative/models/services/guest/AccountService.java
@@ -4,6 +4,7 @@ import by.bntu.fitr.povt.bahirauruslan.facultative.models.dao.IDao;
 import by.bntu.fitr.povt.bahirauruslan.facultative.models.dao.JdbcAccountDao;
 import by.bntu.fitr.povt.bahirauruslan.facultative.models.entities.Account;
 
+//import javax.security.enterprise.authentication.mechanism.http.;
 import java.util.List;
 
 public class AccountService {
@@ -16,4 +17,8 @@ public class AccountService {
     public boolean addAccount(Account account) {
         return dao.add(account);
     }
+
+//    private String hashPassword(String password) {
+//        return new Pbkdf2PasswordHash().generate(password.toCharArray());
+//    }
 }
