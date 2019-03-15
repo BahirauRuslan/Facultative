0ac20117009c1a94376ca4382216d1ba3f632c62
Bahirau Ruslan <ruslan.bahirau.1999@gmail.com>
2019-03-15 10:51:15 +0300

    Added authentificationResult enum

---
+++ src/main/java/by/bntu/fitr/povt/bahirauruslan/facultative/models/util/authentication/AuthenticationResult.java
@@ -0,0 +1,7 @@
+package by.bntu.fitr.povt.bahirauruslan.facultative.models.util.authentication;
+
+public enum AuthenticationResult {
+    USER_DOES_NOT_EXIST,
+    PASSWORD_CHECK_FAILED,
+    OK
+}
